5e8b9248b285d47716661462b59f4d2c97dbb3b2
VinitShah2411 <vinitshah2411@gmail.com>
2022-08-22 12:00:20 +0530

    done

---
+++ src/app/signup/signup.component.ts
@@ -0,0 +1,19 @@
+import { Component, ComponentFactoryResolver} from '@angular/core';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.css']
+})
+export class SignupComponent  {
+
+  firstName:String=""
+  email="" // auto - string 
+  password:String=""
+  
+printData(){
+  console.log(this.firstName)
+  console.log(this.email);
+  console.log(this.password);
+}
+}
